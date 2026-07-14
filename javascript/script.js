4c891003f94980f1bce53f42f6958e21f3ee5be9
Cristian Diaz <cristian.d5299@gmail.com>
2026-07-13 22:14:37 -0500

    interaction examples

--- javascript/script.js
+++ javascript/script.js
@@ -1,10 +1,9 @@
 alert("Javascript alert with external script")
+let currentUserName = "Cristian";
+alert(`El valor de la variable es, ${currentUserName}`);
 
-let admin;
-let name;
-name = "John";
-admin = name;
-alert(admin);
-
-let humanPlanet = "earth";
-let currentUserName = "John";
+let suggestedAgeText = "Ingresa tu edad."
+let userAge = prompt(suggestedAgeText, 100);
+alert(`Tu edad es ${userAge}`)
+let confirmation = confirm("Are you sure?")
+alert(`Your confirmation is ${confirmation}`)
